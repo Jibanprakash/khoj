856f9fb84883a373c131fb05ddf83e62e780396c
Saurabh Araiyer <sizzsa@gmail,com>
2014-10-03 17:45:54 +0530

    Updating License
    Adding GNU GPL Version 3+

--- src/lookupFile.cpp
+++ src/lookupFile.cpp
@@ -1,3 +1,27 @@
+/*
+ * This program is a part of Khoj - The multi-platform Local search engine
+ *
+ * Copyright (C) 2014 Jiban Prakash, jibanprakash.jp(at)gmail(dot)com
+ * Saurabh Araiyer,  sizzsa(at)gmail(dot)com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+ /*
+  * This program implements the engine to parse and find the required file
+  */
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
